0568f14bd64e6a70ef2ed5200ba6f039df1f7d43
Nevada <olis0001@yandex.ru>
2023-11-12 22:02:56 +0700

    test jwt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -125,6 +125,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return Users.findUserByCredentials(email, password)
     .then((user) => {
+      console.log(JWT_SECRET);
       res.send({
         token: jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' }),
       });
