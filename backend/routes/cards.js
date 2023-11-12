78c3f9597a0498f4dbf9488054b51292d4dcac54
Nevada <olis0001@yandex.ru>
2023-11-12 22:49:35 +0700

    add joi validation in cards

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -20,7 +20,13 @@ cardsRoutes.delete(
 );
 cardsRoutes.post(
   '/',
-
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).required(),
+      link: Joi.string().min(2).max(30).required()
+        .regex(/https?:\/\/[a-zA-Z0-9]+\.[^\s]{2,}/),
+    }),
+  }),
   createCard,
 );
 cardsRoutes.put(
